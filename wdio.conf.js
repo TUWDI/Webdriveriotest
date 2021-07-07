5f8ba0e9dafa8f663206aceb82726843a5f29473
Kravch773 <87003976+Kravch773@users.noreply.github.com>
2021-07-07 11:57:58 +0300

    19

--- wdio.conf.js
+++ wdio.conf.js
@@ -65,10 +65,8 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: "chrome",
-        chromeOptions: {
-            args: ['--disable-gpu', '--no-sandbox', '--disable-extensions', '--disable-dev-shm-usage']
-          },
+        browserName: "firefox",
+     
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
@@ -122,7 +120,15 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services:  ['firefox-profile', {
+        extensions: [
+            '/path/to/extensionA.xpi', // path to .xpi file
+            '/path/to/extensionB' // or path to unpacked Firefox extension
+        ],
+        'xpinstall.signatures.required': false,
+        'browser.startup.homepage': 'https://webdriver.io',
+        legacy: true // only use for firefox <= 55
+    }],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
